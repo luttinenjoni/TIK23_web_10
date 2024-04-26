e30e425a6331a947ae46c1f404d643027721f3a4
luttinenjoni <luttinenjoni@gmail.com>
2024-04-26 10:34:44 +0300

    Update funktiot.js

--- web_10_TIK23SP/joni_jako/funktiot.js
+++ web_10_TIK23SP/joni_jako/funktiot.js
@@ -1,7 +1,9 @@
 const randomnro1 = getRandomInt(10)
 const randomnro2 = 2
-const kirjoitettuTulos = document.getElementById('inputnro').innerHTML
+const kirjoitettuTulos = 
 const lasku = randomnro1 / randomnro2
+
+document.addEventListener
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
