f817acf02ed0311d3ede8800f6b4292691cddcf4
luttinenjoni <luttinenjoni@gmail.com>
2024-05-01 10:31:12 +0300

    jakolaskutehtäviä

--- web_10_TIK23SP/joni_jako/funktiot.js
+++ web_10_TIK23SP/joni_jako/funktiot.js
@@ -1,12 +1,21 @@
 const randomnro1 = getRandomInt(10)
 const randomnro2 = 2
-const kirjoitettuTulos = 
 const lasku = randomnro1 / randomnro2
+const button = document.getElementById("button")
+const vastaus = document.getElementById("vastaus")
 
-document.addEventListener
+
+button.addEventListener('click', () =>
+  {
+    if (vastaus.value = lasku)
+      alert("Oikein")
+    else
+      alert("Väärin vitun nuija")
+  }
+)
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-document.getElementById('laskut').innerHTML = kirjoitettuTulos
+document.getElementById('laskut').innerHTML = randomnro1 + " / " + randomnro2 + " ="
